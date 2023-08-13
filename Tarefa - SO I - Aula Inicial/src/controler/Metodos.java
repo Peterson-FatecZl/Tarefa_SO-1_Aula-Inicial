197aa531091724c003d24c039b3d3dcca9123b83
Peterson-FatecZl <141875622+Peterson-FatecZl@users.noreply.github.com>
2023-08-13 15:56:06 -0300

    Add files via upload

---
+++ Tarefa - SO I - Aula Inicial/src/controler/Metodos.java
@@ -0,0 +1,16 @@
+package controler;
+
+public class Metodos {
+
+	public Metodos() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	public int[] preencherVetor(int[] vetor) {
+		for (int ctd = 0; ctd < vetor.length; ctd++) {
+			vetor[ctd] = 0;
+		}
+		return vetor;
+	}
+
+}
